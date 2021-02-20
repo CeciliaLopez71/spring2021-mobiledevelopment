52ff46c94448c38a6ec37801532a1364352724f1
CeciliaLopez139 <Cecilialopez139@users.noreply.github.com>
2021-02-19 22:41:08 -0500

    Added Example component to App.js

---
+++ week6/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Example from './components/Example/index.js';
+
+function App() {
+  return (
+   
+    <div className="App">
+       <Example/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ week6/src/components/Example/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Example = () => {
+    return (
+        <div>
+
+            <p>Hi, my name is Cecilia Lopez.</p>
+            
+        </div>
+    )
+}
+
+export default Example
